8613502d4a270c052f39c347d0b4bdcfba5c6484
Hridayash Pariyar <hridayashpariyar@Hridayashs-MacBook-Pro.local>
2024-02-07 00:13:38 -0500

    update portfolio section

--- Frontend/src/Components/portfolioCard.jsx
+++ Frontend/src/Components/portfolioCard.jsx
@@ -4,12 +4,12 @@ export default function PortfolioCard(){
     return(
         <>
         <div className="relative min-h-screen  w-full">
-        <h1 className="text-8xl font-semibold text-center m-10">SELECTED WORKS</h1>
+            
   
             <h1 className="text-9xl font-extrabold absolute bottom-0 left-10 ">CLASSIC <br/>GAME</h1>
-            <span className="h-96 w-72 absolute bottom-0 left-2/4 top-32 ">
-                <img src={bg} className="h-96 w-72 rounded-lg relative"/>
-                <img src={game} className=" rounded-lg w-96 h-48 absolute top-20 -right-10"/>  
+            <span className=" w-72 h-96 absolute bottom-0 left-2/4 top-32 flex flex-col justify-center items-center rounded-md" style={{ backgroundImage: `url(${bg})` }}>
+                {/* <img src={bg} className="h-96 w-72 rounded-lg relative"/> */}
+                <img src={game} className=" rounded-lg w-96 h-48  "/>  
             </span>
              
             <p className="text-left absolute bottom-0 right-10">THE DESCRIPTION OF THE CLASSIC <br/>GAME ARE AS FOLLOW AND I DONT <br/>KNOW WHAT TO SAY</p>
